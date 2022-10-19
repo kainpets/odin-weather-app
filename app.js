9ab369531ddb4c2435b78a9d86e7fd8ff8d00324
kainpets <pawelstepniak22@gmail.com>
2022-10-19 20:34:05 +0200

    location is now fetched from user's input

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-async function fetchWeather() {
+async function fetchWeather(location) {
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=119ed74a23e4f5335ac1fb44359fe2f3`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=119ed74a23e4f5335ac1fb44359fe2f3`
   );
   const json = await response.json();
   console.log(json);
 }
-
-fetchWeather();
 
 function getLocation() {
   const searchBar = document.querySelector("#search-bar");
@@ -14,7 +12,7 @@ function getLocation() {
   submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const location = searchBar.value;
-    console.log(location);
+    fetchWeather(location);
   });
 }
 
