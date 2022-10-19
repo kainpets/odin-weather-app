9243c85f9d4b8472be1b63256ac18bb70cef44d0
kainpets <pawelstepniak22@gmail.com>
2022-10-19 20:48:44 +0200

    weather can now be fetched from altitude and longitude

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 async function fetchWeather(location) {
+  // get longititude and latitiude
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=119ed74a23e4f5335ac1fb44359fe2f3`
   );
   const json = await response.json();
-  console.log(json);
+  const lon = json[0].lon;
+  const lat = json[0].lat;
+  console.log(lon, lat);
+
+  // get the weather
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=119ed74a23e4f5335ac1fb44359fe2f3`
+  );
+  const jsonWeather = await weatherResponse.json();
+  console.log(jsonWeather);
 }
 
 function getLocation() {
